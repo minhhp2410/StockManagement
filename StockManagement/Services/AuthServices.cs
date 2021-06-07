af50cd2bb6d070a9281f52933cbfc1e2ba6f9503
Minh Dinh <minhdc@datatech.vn>
2021-06-07 16:07:28 +0700

    add login service

---
+++ StockManagement/Services/AuthServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockManagement.Services
+{
+    class AuthServices:Http
+    {
+        public string _login(UserLogin user)
+        {
+
+            return ((Model.GetToken)Post(env.authorizePath, user, typeof(UserLogin))).message;
+        }
+    }
+}
